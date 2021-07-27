2f26eb3f8c759a4db0dabf46dff0b5b1dd789f89
jack242424 <ec2-user@ip-10-0-69-204.ap-northeast-1.compute.internal>
2021-07-27 05:53:54 +0000

    modify twicy

--- app.rb
+++ app.rb
@@ -7,8 +7,16 @@ class MyApp < Sinatra::Application
   
   http_requests = Prometheus::Client::Counter.new(:http_requests, docstring: 'A counter of HTTP requests made')
   prometheus.register(http_requests)
+  
+  viewer_gauge = Prometheus::Client::Gauge.new(:viewers, docstring: 'E_TOOMANY_VIERERS', labels: [:service])
+  prometheus.register(viewer_gauge)
   get '/' do
     http_requests.increment
     'Hello world!'
+  end
+  
+  get '/twitchy' do
+    num_viewers = rand(50)
+    viewer_gauge.set(num_viewers, labels: { service: 'twich' })
   end
 end
