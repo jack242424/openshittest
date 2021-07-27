e3ad3f6f040a0b3cc1ea88a810d9a7ff14dd0215
jack242424 <ec2-user@ip-10-0-69-204.ap-northeast-1.compute.internal>
2021-07-27 05:20:54 +0000

    create app

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra/base'
+require 'prometheus/client'
+
 
 class MyApp < Sinatra::Application
+  prometheus = Prometheus::Client.registry
+  
+  http_requests = Prometheus::Client::Counter.new(:http_requests, docstring: 'A counter of HTTP requests made')
+  prometheus.register(http_requests)
   get '/' do
+    http_requests.increment
     'Hello world!'
   end
 end
